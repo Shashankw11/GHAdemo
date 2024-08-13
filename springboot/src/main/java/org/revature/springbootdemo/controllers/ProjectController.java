56c8a7acb989cfc5722ce5fa0ee04db9c3ff4425
Shashankw11 <shashankwaghale@gmail.com>
2024-08-13 13:08:26 +0530

    corconfig added

--- springboot/src/main/java/org/revature/springbootdemo/controllers/ProjectController.java
+++ springboot/src/main/java/org/revature/springbootdemo/controllers/ProjectController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
