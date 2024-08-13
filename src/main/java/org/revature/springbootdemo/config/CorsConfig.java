388211a475598b13e6be049c6075e1a56e871c28
Shashankw11 <shashankwaghale@gmail.com>
2024-08-13 15:07:23 +0530

    corsConfig added

--- src/main/java/org/revature/springbootdemo/config/CorsConfig.java
+++ src/main/java/org/revature/springbootdemo/config/CorsConfig.java
@@ -8,14 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsConfig() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
